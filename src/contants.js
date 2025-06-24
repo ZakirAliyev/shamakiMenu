7ec230af1f76da12d9fcb43222863c26914c19e0
Zakir Aliyev <aliyevzakir814@gmail.com>
2025-06-24 17:14:47 +0400

    Shamaki

--- src/contants.js
+++ src/contants.js
@@ -1,2 +1,2 @@
-export const CATEGORY_IMAGES = 'https://natavanmenu-001-site1.ltempurl.com/files/category_images/'
-export const PRODUCT_IMAGES = 'https://natavanmenu-001-site1.ltempurl.com/files/product_images/'
+export const CATEGORY_IMAGES = 'https://samaximenuu-001-site1.ltempurl.com/files/category_images/'
+export const PRODUCT_IMAGES = 'https://samaximenuu-001-site1.ltempurl.com/files/product_images/'
